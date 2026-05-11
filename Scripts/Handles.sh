710492992efe4db276b04c9fdcd98068954f7e76
lzw981731 <lzw981731@gmail.com>
2026-05-11 13:36:16 +0800

    fix: 修复WRT_LIST多行环境变量格式错误

---
+++ Scripts/Handles.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+
+PKG_PATH="$GITHUB_WORKSPACE/$WRT_DIR/package/"
+
+#预置HomeProxy数据
+if [ -d *"homeproxy"* ]; then
+	echo " "
+
+	HP_RULE="surge"
+	HP_PATH="homeproxy/root/etc/homeproxy"
+
+	rm -rf ./$HP_PATH/resources/*
+
+	git clone -q --depth=1 --single-branch --branch "release" "https://github.com/Loyalsoldier/surge-rules.git" ./$HP_RULE/
+	cd ./$HP_RULE/ && RES_VER=$(git log -1 --pretty=format:'%s' | grep -o "[0-9]*")
+
+	echo $RES_VER | tee china_ip4.ver china_ip6.ver china_list.ver gfw_list.ver
+	awk -F, '/^IP-CIDR,/{print $2 > "china_ip4.txt"} /^IP-CIDR6,/{print $2 > "china_ip6.txt"}' cncidr.txt
+	sed 's/^\.//g' direct.txt > china_list.txt ; sed 's/^\.//g' gfw.txt > gfw_list.txt
+	mv -f ./{china_*,gfw_list}.{ver,txt} ../$HP_PATH/resources/
+
+	cd .. && rm -rf ./$HP_RULE/
+
+	cd $PKG_PATH && echo "homeproxy date has been updated!"
+fi
+
+#修改argon主题字体和颜色
+if [ -d *"luci-theme-argon"* ]; then
+	echo " "
+
+	cd ./luci-theme-argon/
+
+	sed -i "s/primary '.*'/primary '#31a1a1'/; s/'0.2'/'0.5'/; s/'none'/'bing'/; s/'600'/'normal'/" ./luci-app-argon-config/root/etc/config/argon
+
+	cd $PKG_PATH && echo "theme-argon has been fixed!"
+fi
+
+#修改aurora菜单式样
+if [ -d *"luci-app-aurora-config"* ]; then
+	echo " "
+
+	cd ./luci-app-aurora-config/
+
+	sed -i "s/nav_submenu_type '.*'/nav_submenu_type 'boxed-dropdown'/g" $(find ./root/ -type f -name "*aurora")
+
+	cd $PKG_PATH && echo "theme-aurora has been fixed!"
+fi
+
+#修改qca-nss-drv启动顺序
+NSS_DRV="../feeds/nss_packages/qca-nss-drv/files/qca-nss-drv.init"
+if [ -f "$NSS_DRV" ]; then
+	echo " "
+
+	sed -i 's/START=.*/START=85/g' $NSS_DRV
+
+	cd $PKG_PATH && echo "qca-nss-drv has been fixed!"
+fi
+
+#修改qca-nss-pbuf启动顺序
+NSS_PBUF="./kernel/mac80211/files/qca-nss-pbuf.init"
+if [ -f "$NSS_PBUF" ]; then
+	echo " "
+
+	sed -i 's/START=.*/START=86/g' $NSS_PBUF
+
+	cd $PKG_PATH && echo "qca-nss-pbuf has been fixed!"
+fi
+
+#修复TailScale配置文件冲突
+TS_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/tailscale/Makefile")
+if [ -f "$TS_FILE" ]; then
+	echo " "
+
+	sed -i '/\/files/d' $TS_FILE
+
+	cd $PKG_PATH && echo "tailscale has been fixed!"
+fi
+
+#修复Rust编译失败
+RUST_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/rust/Makefile")
+if [ -f "$RUST_FILE" ]; then
+	echo " "
+
+	sed -i 's/ci-llvm=true/ci-llvm=false/g' $RUST_FILE
+
+	cd $PKG_PATH && echo "rust has been fixed!"
+fi
+
+#修复DiskMan编译失败
+DM_FILE="./luci-app-diskman/applications/luci-app-diskman/Makefile"
+if [ -f "$DM_FILE" ]; then
+	echo " "
+
+	sed -i '/ntfs-3g-utils /d' $DM_FILE
+
+	cd $PKG_PATH && echo "diskman has been fixed!"
+fi
+
+#修复luci-app-netspeedtest相关问题
+if [ -d *"luci-app-netspeedtest"* ]; then
+	echo " "
+
+	cd ./luci-app-netspeedtest/
+
+	sed -i '$a\exit 0' ./netspeedtest/files/99_netspeedtest.defaults
+	sed -i 's/ca-certificates/ca-bundle/g' ./speedtest-cli/Makefile
+
+	cd $PKG_PATH && echo "netspeedtest has been fixed!"
+fi
+
+#修复 mac80211 ath11k 编译错误 (上游 patch 格式损坏)
+MAC80211_PATCH="./kernel/mac80211/patches/ath11k/990-ath11k-clamp-reg-rule-bandwidth.patch"
+if [ -f "$MAC80211_PATCH" ]; then
+	echo " "
+	rm -rf $MAC80211_PATCH
+	cd $PKG_PATH && echo "mac80211 broken patch has been removed!"
+fi
+
+#预先下载 OpenClash 核心，防止编译后界面提示“未安装内核”
+if [ -d *"openclash"* ]; then
+	echo " "
+	echo "Downloading OpenClash cores..."
+	CORE_DIR="$GITHUB_WORKSPACE/$WRT_DIR/files/etc/openclash/core"
+	mkdir -p $CORE_DIR
+
+	# 下载 Meta 内核
+	curl -sL -o $CORE_DIR/clash_meta.tar.gz "https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-armv8.tar.gz"
+	tar -zxf $CORE_DIR/clash_meta.tar.gz -C $CORE_DIR/
+	mv $CORE_DIR/clash $CORE_DIR/clash_meta
+	rm -f $CORE_DIR/clash_meta.tar.gz
+	chmod +x $CORE_DIR/clash_meta
+
+	# 下载 Dev 内核
+	curl -sL -o $CORE_DIR/clash_dev.tar.gz "https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-armv8.tar.gz"
+	tar -zxf $CORE_DIR/clash_dev.tar.gz -C $CORE_DIR/
+	rm -f $CORE_DIR/clash_dev.tar.gz
+	chmod +x $CORE_DIR/clash
+
+	cd $PKG_PATH && echo "OpenClash cores downloaded successfully!"
+fi
